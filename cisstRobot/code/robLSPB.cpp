bbd78317ca2fd7fbea6adaeede0e2a4870bf64d2
Brandon Cohen <branman59x@yahoo.com>
2015-07-10 16:13:16 -0400

    Trajectory Generator code (needs to be able to go backwards)

--- cisstRobot/code/robLSPB.cpp
+++ cisstRobot/code/robLSPB.cpp
@@ -2,7 +2,7 @@
 /* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
 
 /*
-  Author(s):  Anton Deguet
+  Author(s):  Anton Deguet, Brandon Cohen
   Created on: 2014-10-27
 
   (C) Copyright 2014 Johns Hopkins University (JHU), All Rights Reserved.
